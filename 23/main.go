952e00ae74c285f43154cfa213db1dd7ab1189e9
6jodeci <egor0263s@gmail.com>
2022-07-24 21:27:48 +0500

    task#23

---
+++ 23/main.go
@@ -0,0 +1,24 @@
+// Удалить i-ый элемент из слайса.
+package main
+
+import "fmt"
+
+func main() {
+	slice := []string{"A", "B", "C", "D", "E", "F"}
+	fmt.Println("Исходный слайс: ", slice)
+
+	var i int // i-ый элемент
+	fmt.Print("Какой элемент удалить: ")
+	fmt.Scan(&i)
+
+	// 1. Выполнить сдвиг a[i+1:] влево на один индекс.
+	copy(slice[i:], slice[i+1:])
+
+	// удалить последний элемент (записать нулевое значение)
+	slice[len(slice)-1] = ""
+
+	// уменьшить слайс
+	slice = slice[:len(slice)-1]
+
+	fmt.Println("Слайс после удаления i-того элемента: ", slice)
+}
