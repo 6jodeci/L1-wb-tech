088e06e4ae28a4ba9ebb66c851454d23d05afee9
6jodeci <egor0263s@gmail.com>
2022-07-24 13:50:03 +0500

    task#21

--- 21/main.go
+++ 21/main.go
@@ -1,7 +1,30 @@
 // Реализовать паттерн «адаптер» на любом примере.
-
 package main
 
-func main() {
-	
+// Target предоставляет интерфейс, с которым должна работать система
+type Target interface {
+	Request() string
+}
+
+// Adaptee реализует адаптируемую систему.
+type Adaptee struct{}
+
+// NewAdapter - конструктор адаптера.
+func NewAdapter(adaptee *Adaptee) Target {
+	return &Adapter{adaptee}
+}
+
+// SpecificRequest -это реализация конкретного запроса
+func (a *Adaptee) SpecificRequest() string {
+	return "Request"
+}
+
+// Адаптер реализует интерфейс Target и является адаптером.
+type Adapter struct {
+	*Adaptee
+}
+
+// Request - это адаптивный метод.
+func (a *Adapter) Request() string {
+	return a.SpecificRequest()
 }
